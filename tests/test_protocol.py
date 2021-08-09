2bee54ecf19679f2e303c8daeaff541d152c9cf6
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-08-09 23:07:36 +0300

    Add fail test

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -30,3 +30,10 @@ def test_tcp_to_udp_empty():
     """Convert empty UDP datagrams back from TCP."""
     uott_msg = b"UOTT\x00\x00"
     assert protocol.tcp_msg_to_udp_dgram(uott_msg) == b""
+
+
+def test_tcp_to_udp_bad_magic():
+    """Should raise assertion when magic does not match."""
+    with pytest.raises(AssertionError):
+        uott_msg = b"HAHA\x00\x00"
+        protocol.tcp_msg_to_udp_dgram(uott_msg)
