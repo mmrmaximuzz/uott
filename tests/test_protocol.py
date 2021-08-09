115038a8ecc6a8193af46b4ebea33f52346f1256
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-08-09 23:12:03 +0300

    Siplify names

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -7,40 +7,40 @@ from uott import protocol
 
 def test_udp_to_tcp_empty():
     """UDP datagrams can be zero-sized."""
-    udp_dgram = b""
-    uott_msg = protocol.udp_dgram_to_tcp_msg(udp_dgram)
-    assert uott_msg == protocol.MAGIC + (0).to_bytes(2, "little")
+    dgram = b""
+    msg = protocol.udp_dgram_to_tcp_msg(dgram)
+    assert msg == protocol.MAGIC + (0).to_bytes(2, "little")
 
 
 def test_udp_to_tcp_normal():
     """Test ordinary datagrams."""
-    udp_dgram = b"0123456789abcdef"
-    uott_msg = protocol.udp_dgram_to_tcp_msg(udp_dgram)
-    assert uott_msg == protocol.MAGIC + (16).to_bytes(2, "little") + udp_dgram
+    dgram = b"0123456789abcdef"
+    msg = protocol.udp_dgram_to_tcp_msg(dgram)
+    assert msg == protocol.MAGIC + (16).to_bytes(2, "little") + dgram
 
 
 def test_udp_to_tcp_oversized():
     """Should raise assertion on oversized datagrams."""
     with pytest.raises(AssertionError):
-        udp_dgram = b"0" * 65536
-        protocol.udp_dgram_to_tcp_msg(udp_dgram)
+        dgram = b"0" * 65536
+        protocol.udp_dgram_to_tcp_msg(dgram)
 
 
 def test_tcp_to_udp_empty():
     """Convert empty UDP datagrams back from TCP."""
-    uott_msg = b"UOTT\x00\x00"
-    assert protocol.tcp_msg_to_udp_dgram(uott_msg) == b""
+    msg = b"UOTT\x00\x00"
+    assert protocol.tcp_msg_to_udp_dgram(msg) == b""
 
 
 def test_tcp_to_udp_bad_magic():
     """Should raise assertion when magic does not match."""
     with pytest.raises(AssertionError):
-        uott_msg = b"HAHA\x00\x00"
-        protocol.tcp_msg_to_udp_dgram(uott_msg)
+        msg = b"HAHA\x00\x00"
+        protocol.tcp_msg_to_udp_dgram(msg)
 
 
 def test_tcp_to_udp_too_short():
     """Should raise assertion when the message is too short."""
     with pytest.raises(AssertionError):
-        uott_msg = b"UOTT\x00"
-        protocol.tcp_msg_to_udp_dgram(uott_msg)
+        msg = b"UOTT\x00"
+        protocol.tcp_msg_to_udp_dgram(msg)
