31be46f2cef058963801ea15b4db7da66c3c081b
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-08-09 22:32:24 +0300

    Add functions stub

--- src/uott/protocol.py
+++ src/uott/protocol.py
@@ -1,3 +1,11 @@
 """protocol - base definitions for uott protocol."""
 
 UOTT_MAGIC = b"UOTT"
+
+
+def udp_dgram_to_tcp_msg():
+    """Convert UDP datagram to UOTT TCP message."""
+
+
+def tcp_msg_to_udp_dgram():
+    """Convert UOTT TCP message to UDP datagram."""
