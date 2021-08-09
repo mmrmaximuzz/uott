e0f5bb4f42c7f7f0b17a9791da6031d37cf14e6b
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-08-09 23:15:43 +0300

    Another fail test

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -44,3 +44,10 @@ def test_tcp_to_udp_too_short():
     with pytest.raises(AssertionError):
         msg = b"UOTT\x00"
         protocol.tcp_msg_to_udp_dgram(msg)
+
+
+def test_tcp_to_udp_dgram_len_short():
+    """Should raise assertion when the dgram len lesser than msg field."""
+    with pytest.raises(AssertionError):
+        msg = b"UOTT\x03\x00\xff\xff"
+        protocol.tcp_msg_to_udp_dgram(msg)
