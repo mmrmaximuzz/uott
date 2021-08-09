b666acbf5aa81202c07fa47b02cccf23cc3e1974
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-08-09 22:53:17 +0300

    Add assertion test

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,5 +1,7 @@
 """Testing suite for uott protocol module."""
 
+import pytest
+
 from uott import protocol
 
 
@@ -15,3 +17,10 @@ def test_udp_to_tcp_normal():
     udp_dgram = b"0123456789abcdef"
     uott_msg = protocol.udp_dgram_to_tcp_msg(udp_dgram)
     assert uott_msg == protocol.MAGIC + (16).to_bytes(2, "little") + udp_dgram
+
+
+def test_udp_to_tcp_oversized():
+    """Should raise assertion on oversized datagrams."""
+    with pytest.raises(AssertionError):
+        udp_dgram = b"0" * 65536
+        protocol.udp_dgram_to_tcp_msg(udp_dgram)
