dfc9f7d00981995a461acd2ce8dd741bcbc159e2
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-08-09 22:51:17 +0300

    Ordinary test

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -8,3 +8,10 @@ def test_udp_to_tcp_empty():
     udp_dgram = b""
     uott_msg = protocol.udp_dgram_to_tcp_msg(udp_dgram)
     assert uott_msg == protocol.MAGIC + (0).to_bytes(2, "little")
+
+
+def test_udp_to_tcp_normal():
+    """Test ordinary datagrams."""
+    udp_dgram = b"0123456789abcdef"
+    uott_msg = protocol.udp_dgram_to_tcp_msg(udp_dgram)
+    assert uott_msg == protocol.MAGIC + (16).to_bytes(2, "little") + udp_dgram
