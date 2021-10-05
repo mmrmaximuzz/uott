c8278144775c6fb503a6e4a13c0d836bb1573bec
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-10-05 19:04:08 +0300

    Add assert message

--- src/uott/protocol.py
+++ src/uott/protocol.py
@@ -8,7 +8,7 @@ BYTEORDER = "little"
 def udp_dgram_to_tcp_msg(dgram: bytes) -> bytes:
     """Convert UDP datagram to UOTT TCP message."""
     udp_len = len(dgram)
-    assert udp_len <= 0xFFFF
+    assert udp_len <= 0xFFFF, "oversized UDP datagram"
 
     return MAGIC + udp_len.to_bytes(LEN_BYTES, BYTEORDER) + dgram
 
