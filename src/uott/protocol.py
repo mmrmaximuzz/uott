6ae3acb2219603729bc65ddbe16e7376653ef198
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-10-10 20:05:03 +0300

    Rework protocol module structure

--- src/uott/protocol.py
+++ src/uott/protocol.py
@@ -1,47 +1,36 @@
 """protocol - base definitions for uott protocol."""
 
-from typing import Optional, Tuple
+from typing import Generator, Optional, Tuple
 
 from .utils import split_at
 
-MAGIC = b"UOTT"
-TAG_BYTES = 2
-LEN_BYTES = 2
-BYTEORDER = "little"
+_MAGIC = b"UOTT"
+_TAG_BYTES = 2
+_LEN_BYTES = 2
+_BYTEORDER = "little"
+
+UOTTTag = int
+UOTTData = bytes
+UOTTMsg = Tuple[UOTTTag, UOTTData]
+StreamTransformer = Generator[bytes, Optional[UOTTMsg], None]
 
 
-def udp_dgram_to_tcp_msg(dgram: bytes, tag: int) -> bytes:
+def udp_dgram_to_tcp_msg(uott: UOTTMsg) -> bytes:
     """Convert UDP datagram to UOTT TCP message with given tag."""
-    dlen = len(dgram)
+    tag, data = uott
+
+    dlen = len(data)
     assert dlen <= 0xFFFF, f"oversized UDP datagram: {dlen} B"
     assert tag <= 0xFFFF, f"tag ID overflow: {tag}"
 
     parts = [
-        MAGIC,
-        tag.to_bytes(TAG_BYTES, BYTEORDER),
-        dlen.to_bytes(LEN_BYTES, BYTEORDER),
-        dgram,
+        _MAGIC,
+        tag.to_bytes(_TAG_BYTES, _BYTEORDER),
+        dlen.to_bytes(_LEN_BYTES, _BYTEORDER),
+        data,
     ]
     return b"".join(parts)
 
 
-def tcp_msg_to_udp_dgram(msg: bytes) -> Optional[Tuple[bytes, bytes]]:
-    """Convert UOTT TCP message to UDP datagram."""
-    if len(msg) < len(MAGIC):
-        # magic is not complete
-        return None
-
-    magic, msg = split_at(msg, len(MAGIC))
-    assert magic == MAGIC, f"UOTT protocol is corrupted: wrong magic {magic}"
-
-    if len(msg) < LEN_BYTES:
-        # length is not complete
-        return None
-
-    udp_len_bytes, msg = split_at(msg, LEN_BYTES)
-    udp_len = int.from_bytes(udp_len_bytes, BYTEORDER)
-    if len(msg) < udp_len:
-        # message is not complete
-        return None
-
-    return split_at(msg, udp_len)
+def tcp_msg_to_udp_dgram_transformer() -> StreamTransformer:
+    """Convert UOTT TCP message to UDP datagram in a coroutine."""
