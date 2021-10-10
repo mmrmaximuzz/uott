3a40a3d068f0915a2d2bd683a6e16b4469791161
Maxim Petrov <mmrmaximuzz@gmail.com>
2021-10-10 18:47:38 +0300

    Add raw version for argument parsing

--- src/uott/__main__.py
+++ src/uott/__main__.py
@@ -1,9 +1,30 @@
-"""Daemon launcher for uott."""
+"""Launcher for UOTT."""
+
+import argparse
+
+
+def parse_cli_args() -> argparse.Namespace:
+    """Parse CLI arguments when called from command line."""
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(help="UOTT mode", dest="mode")
+
+    proxy = subparsers.add_parser("proxy", help="run UOTT in proxy mode")
+    proxy.add_argument("local-udp")
+    proxy.add_argument("remote-tcp")
+
+    client = subparsers.add_parser("client", help="run UOTT in client mode")
+    client.set_defaults(mode="client")
+    client.add_argument("local-tcp")
+    client.add_argument("remote-udp")
+
+    return parser.parse_args()
 
 
 def main() -> None:
-    """Entry point for uott daemon."""
-    print("hello world")
+    """Entry point for UOTT daemon."""
+    opts = parse_cli_args()
+    print(opts)
 
 
 if __name__ == "__main__":
